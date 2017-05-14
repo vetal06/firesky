48f6239ee6fa6fe4b947381336ef2b564e1cc1c5
Skripnik <bbssvv06@gmail.com>
2017-05-14 18:35:16 +0300

    left-menu widget cache

--- basic/widgets/menu/LeftMenu.php
+++ basic/widgets/menu/LeftMenu.php
@@ -10,8 +10,15 @@ class LeftMenu extends \yii\base\Widget
 
   public function run()
   {
-    $categories = Category::find()->where('lvl = 1')->orderBy('lft')->all();
-    return $this->render('left-menu', compact('categories'));
+    $key =  self::className();
+    $res = \Yii::$app->cache->get($key);
+    if (!$res) {
+      $categories = Category::find()->where('lvl = 1')->orderBy('lft')->all();
+      $res = $this->render('left-menu', compact('categories'));
+      \Yii::$app->cache->set($key, $res);
+    }
+    return $res;
+
   }
 
   public function writeMenu($categories,  callable $callback, $isChildren = false)
